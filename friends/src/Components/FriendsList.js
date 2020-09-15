028c8bcc5362e022d9c795e62b8b067a85f48f07
joel vega <joelvega97@yahoo.com>
2020-09-15 18:40:02 -0400

    Made routes and Links to each respective routed component.

---
+++ friends/src/Components/FriendsList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function FriendsList() {
+  return (
+    <>
+      <h2>Here are All your friends!!</h2>
+    </>
+  );
+}
---
+++ friends/src/Components/Opener.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Opener() {
+  return (
+    <>
+      <div>
+        <h2>Who are your friends?</h2>
+      </div>
+    </>
+  );
+}
